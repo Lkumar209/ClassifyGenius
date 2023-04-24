c8a3b30b7b7c37e5c75261b10259f6b6b09c42ba
Laxya Kumar <112336121+Lkumar209@users.noreply.github.com>
2023-04-23 17:09:39 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,34 +1,4 @@
-# import streamlit as st
-# from sklearn import datasets
-# import numpy as np
-# st.title("Streamlit Example")
-
-# st.write("""
-# # Explore different classifier
-# Which one is the best?
-#  """)
-
-# dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine Dataset"))
-# st.write(dataset_name)
-
-
-# classifier_name =  st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
-#  #3 popular ML algorithms: K-nearest neaighbors algo(KNN) , SVM algorithm for vector machines and Random Forest
-
-# def get_dataset(dataset_name):
-#     if dataset_name == "Iris":
-#         data = datasets.load_iris()
-#     elif dataset_name == "Breast Cancer":
-#         data = datasets.load_breast_cancer()
-#     else:
-#         data = datasets.load_wine()
-#     X = data.data
-#     y = data.target
-#     return X, y
-# X, y = get_dataset(dataset_name)
-# st.write("Shape of dataset", X.shape)
-# st.write('number of classes:', len(np.unique(y)))
-
+ 
 
 import streamlit as st 
 import numpy as np 
